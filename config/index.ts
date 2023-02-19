f0ba5235919cf9137ffe82b7a18929d6e6402c07
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-02-19 11:32:34 +0100

    adds environment configuration to the project

---
+++ config/index.ts
@@ -0,0 +1,15 @@
+import {
+  FB_APIKEY,
+  FB_AUTHDOMAIN,
+  FB_PROJECTID,
+  FB_STORAGEBUCKET,
+  FB_MESSAGINGSENDERID,
+  FB_APPID,
+} from "@env";
+
+export const apiKey = FB_APIKEY;
+export const authDomain = FB_AUTHDOMAIN;
+export const projectId = FB_PROJECTID;
+export const storageBucket = FB_STORAGEBUCKET;
+export const messagingSenderId = FB_MESSAGINGSENDERID;
+export const appId = FB_APPID;
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <View>
+      <Text>Grab your</Text>
+      <Text>Delicious meal</Text>
+    </View>
+  );
+};
